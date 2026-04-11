269356ae793cd7d73509be6ffba930e6ea2bbd4f
topliff <brad@topliff.com>
2026-04-11 08:58:24 -0700

    Add Intercom widget (intercom.js) with app_id pj52grij

---
+++ intercom.js
@@ -0,0 +1,29 @@
+// Intercom Messenger Widget
+window.intercomSettings = {
+  api_base: "https://api-ius.intercom.io",
+  app_id: "pj52grij"
+};
+
+(function(){
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic('reattach_activator');
+    ic('update', w.intercomSettings);
+  } else {
+    var d = document;
+    var i = function(){ i.c(arguments); };
+    i.q = [];
+    i.c = function(args){ i.q.push(args); };
+    w.Intercom = i;
+    var l = function(){
+      var s = d.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = 'https://widget.intercom.io/widget/pj52grij';
+      var x = d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    l();
+  }
+})();
